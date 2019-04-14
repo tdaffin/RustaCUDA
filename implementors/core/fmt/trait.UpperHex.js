d35009bf9faf594377ce36308b9342bf2d489d40
Travis CI User <travis@example.org>
2019-04-14 03:55:17 +0000

    Documentation for bheisler/RustaCUDA@2205c42

---
+++ implementors/core/fmt/trait.UpperHex.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["rustacuda"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"rustacuda/context/struct.ContextFlags.html\" title=\"struct rustacuda::context::ContextFlags\">ContextFlags</a>",synthetic:false,types:["rustacuda::context::ContextFlags"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"rustacuda/memory/array/struct.ArrayObjectFlags.html\" title=\"struct rustacuda::memory::array::ArrayObjectFlags\">ArrayObjectFlags</a>",synthetic:false,types:["rustacuda::memory::array::ArrayObjectFlags"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"rustacuda/stream/struct.StreamFlags.html\" title=\"struct rustacuda::stream::StreamFlags\">StreamFlags</a>",synthetic:false,types:["rustacuda::stream::StreamFlags"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"rustacuda/struct.CudaFlags.html\" title=\"struct rustacuda::CudaFlags\">CudaFlags</a>",synthetic:false,types:["rustacuda::CudaFlags"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
